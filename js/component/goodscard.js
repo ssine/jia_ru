aee7817cbc178a449689e723e7523f1fa0129dcf
clpl <825832365@qq.com>
2018-12-21 10:22:14 +0800

    modify header.js

---
+++ js/component/goodscard.js
@@ -0,0 +1,11 @@
+
+
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    };
+  }
+}
+</script>
--- js/header.js
+++ js/header.js
@@ -1,3 +1,22 @@
-document.write('<link rel="stylesheet" href="https://unpkg.com/element-ui@2.4.11/lib/theme-chalk/index.css">')
-document.write('<script src="https://unpkg.com/vue/dist/vue.js"></script>')
-document.write('<script src="https://unpkg.com/element-ui/lib/index.js"></script>')
+var css_cdn = [
+    "https://unpkg.com/element-ui@2.4.11/lib/theme-chalk/index.css",
+    "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css",
+
+];
+var js_cdn = [
+    "https://unpkg.com/vue/dist/vue.js",
+    //element-ui
+    "https://unpkg.com/element-ui/lib/index.js",
+    "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js",
+    "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"
+];
+
+for(var i in css_cdn){
+    link_label = '<link rel="stylesheet" href=\"' +  css_cdn[i] + '\">';
+    document.write(link_label)
+}
+
+for(var i in js_cdn){
+    link_label = '<script src=\"' +  js_cdn[i] + '\"></script>';
+    document.write(link_label)
+}
