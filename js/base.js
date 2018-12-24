5338c069e2c0ef2b4256ddc19c43a6e703e22da3
clpl <825832365@qq.com>
2018-12-24 23:17:43 +0800

    add ajax function

---
+++ js/base.js
@@ -0,0 +1,6 @@
+//全局函数，ajax的封装，目前从json中获取内容，之后改为后端获取
+function g_ajax_post(url, post_data)
+{
+    return_data = back_end_data[url];
+    return return_data;
+}
--- js/header.js
+++ js/header.js
@@ -1,15 +1,18 @@
 var css_cdn = [
-    "https://cdn.bootcss.com/element-ui/2.4.11/index.js",
+    
     "https://cdn.bootcss.com/element-ui/2.4.11/theme-chalk/index.css",
     "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css",
 
 ];
 var js_cdn = [
-    "https://unpkg.com/vue/dist/vue.js",
+    "https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
+    "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js",
     //element-ui
-    "https://unpkg.com/element-ui/lib/index.js",
+    "https://cdn.bootcss.com/element-ui/2.4.11/index.js",
     "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js",
-    "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"
+    
+    "js/back_end_data.json",
+    "js/base.js",
 ];
 
 for(var i in css_cdn){
