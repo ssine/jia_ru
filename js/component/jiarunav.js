9629b2b276f0d89c58d5e0cfb1f6c9122b7628a6
sine <963366202@qq.com>
2019-01-03 21:50:02 +0800

    跳转与表单验证

--- js/component/jiarunav.js
+++ js/component/jiarunav.js
@@ -1,10 +1,10 @@
 Vue.component('jiarunav', {
     template: '<div class="ui menu">\
     <div class="header item"></div>\
-    <a class="item">我要租房</a>\
-    <a class="item">我要出租</a>\
+    <a class="item" onclick="window.open(\'./allhouse.html\', \'_self\')">我要租房</a>\
+    <a class="item" onclick="window.open(\'./house.html\', \'_self\')">我要出租</a>\
       <div class="right menu">\
-      <a class="item">个人面板</a>\
+      <a class="item" onclick="window.open(\'./user-info.html\', \'_self\')">个人面板</a>\
     </div>\
     </div>',
     data: function () {
