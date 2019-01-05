8d6b2e31748a2ba6703129e80b15dc182a065082
sine <963366202@qq.com>
2019-01-05 15:11:36 +0800

    1

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,32 @@
+<template>
+    <div class="index">
+        <Row type="flex" justify="center" align="middle">
+            <Col span="24">
+                <h1>
+                    <img src="../images/logo.png">
+                </h1>
+                <h2>
+                    <p>Welcome to your iView app!</p>
+                    <Button @click="handleStart">Start iView</Button>
+                </h2>
+            </Col>
+        </Row>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleStart () {
+                this.$Modal.info({
+                    title: 'Bravo',
+                    content: 'Now, enjoy the convenience of iView.'
+                });
+            }
+        }
+    }
+</script>
+
 <style scoped lang="less">
     .index{
         width: 100%;
@@ -24,30 +53,3 @@
         }
     }
 </style>
-<template>
-    <div class="index">
-        <Row type="flex" justify="center" align="middle">
-            <Col span="24">
-                <h1>
-                    <img src="../images/logo.png">
-                </h1>
-                <h2>
-                    <p>Welcome to your iView app!</p>
-                    <Button @click="handleStart">Start iView</Button>
-                </h2>
-            </Col>
-        </Row>
-    </div>
-</template>
-<script>
-    export default {
-        methods: {
-            handleStart () {
-                this.$Modal.info({
-                    title: 'Bravo',
-                    content: 'Now, enjoy the convenience of iView.'
-                });
-            }
-        }
-    }
-</script>
