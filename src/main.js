7a724c628c46f1acb12e85a4928a8695bfb552d5
clpl <825832365@qq.com>
2019-01-05 17:02:30 +0800

    update goodslist

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import VueRouter from 'vue-router';
 import Routers from './router';
 import Util from './libs/util';
 import App from './app.vue';
+import Vuex from 'vuex';
 import 'iview/dist/styles/iview.css';
 
 Vue.use(VueRouter);
 Vue.use(iView);
+Vue.use(Vuex);
 
 // 路由配置
 const RouterConfig = {
--- src/views/allhouse.vue
+++ src/views/allhouse.vue
@@ -1,12 +1,17 @@
 <template>
+    <div>
     <jiarunav></jiarunav>
+    <GoodsList></GoodsList>
+    </div>
 </template>
 
 <script>
     import jiarunav from '@/components/jiarunav.vue';
+    import GoodsList from '@/components/GoodsList';
     export default {
         components:{
             jiarunav,
+            GoodsList,
         },
         name: "allhouse"
     }
